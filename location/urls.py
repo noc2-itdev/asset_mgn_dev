f1418e24c837ec2b3095d880877577b677e46c0f
ducdung <ducdung0909@gmail.com>
2026-01-10 15:12:31 +0700

    thay đổi location urls

--- location/urls.py
+++ location/urls.py
@@ -12,6 +12,7 @@ app_name = "location"
 urlpatterns = [
     # liệt kê tất cả location trong database
     path("", views.location_list, name="location-list"),
+    path("create/", views.location_create, name="location-create"),
     # Đường dẫn cho danh sách vị trí lắp đặt và tạo mới
     # Đường dẫn cho chi tiết, cập nhật, xóa vị trí lắp đặt theo ID
     # Đường dẫn cho chi tiết vị trí lắp đặt theo tên
