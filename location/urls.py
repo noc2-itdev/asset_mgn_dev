f90bf237ec4d3d711c11881bf8aafee41f738af7
ducdung <ducdung0909@gmail.com>
2026-01-10 15:11:36 +0700

    thêm code urls list tất cả location

--- location/urls.py
+++ location/urls.py
@@ -2,16 +2,17 @@
 URL patterns cho ứng dụng location
 Định nghĩa các đường dẫn URL cho các view trong ứng dụng
 """
+
 from django.urls import path
 from . import views
 
 # Tên namespace cho ứng dụng location
-app_name = 'location'
+app_name = "location"
 
 urlpatterns = [
+    # liệt kê tất cả location trong database
+    path("", views.location_list, name="location-list"),
     # Đường dẫn cho danh sách vị trí lắp đặt và tạo mới
-    
     # Đường dẫn cho chi tiết, cập nhật, xóa vị trí lắp đặt theo ID
-    
     # Đường dẫn cho chi tiết vị trí lắp đặt theo tên
 ]
