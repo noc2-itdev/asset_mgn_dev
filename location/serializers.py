160f19312cd828e4a255e088872791221ffb84f7
thanhtoan2803 <thanhtoandtvt@gmail.com>
2026-01-11 17:57:38 +0700

    cập nhật file Location>serializer

--- location/serializers.py
+++ location/serializers.py
@@ -11,4 +11,29 @@ class LocationSerializer(serializers.ModelSerializer):
     Serializer cho model Location
     Chuyển đổi dữ liệu giữa model và JSON format
     """
+    class Meta:
+        model = Location
+        fields = '__all__'
+    def validate_name(self, value):
+        """
+        Kiểm tra địa điểm lắp đặt tài sản có tồn tại chưa
 
+        Trường hợp CREATE:
+
+        
+        :param self: Description
+        :param value: Description
+        """
+        # Lấy instance hiện tại
+        instance = self.instance
+        # Kiểm tra có địa điểm lắp đặt có tồn tại chưa
+        # Nếu đang cập nhật, bỏ qua chính instance hiện tại
+        queryset = Location.objects.filter(name__iexact=value)
+        if instance is not None:
+            queryset = queryset.exclude(pk=instance.pk)
+
+        if queryset.exists():
+            raise serializers.ValidationError(f"Địa điểm với tên '{value}' đã tồn tại.")
+        
+        return value
+     
